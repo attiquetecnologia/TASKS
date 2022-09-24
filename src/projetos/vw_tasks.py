698b0e1327e7bfeae2a3b41c024e2eca1b8c84a4
attiquetecnologia <rodrigoatique@gmail.com>
2022-09-24 14:27:28 -0300

    editar dados

--- src/projetos/vw_tasks.py
+++ src/projetos/vw_tasks.py
@@ -4,25 +4,39 @@ from . models import Project, Task
 from app import db
 bp = Blueprint('task', __name__)
 
-@bp.route("/times/add", methods=("GET", ))
+@bp.route("/tasks/add", methods=("GET", ))
 def add():
     projects = Project.query.all()
     return render_template("projetos/tarefas/form.html", projects=projects)
 
+@bp.route("/tasks/edit/<int:id>", methods=("GET", ))
+def edit(id):
+    record = Task.query.filter_by(id=id).first()
+    projects = Project.query.all()
+    return render_template("projetos/tarefas/form.html", record=record, projects=projects)
+
 
 @bp.route("/tasks/form", methods=("POST", ))
 def form():
-    data = {
-        "task_name": request.form.get("task_name")
-        ,"project_id": request.form.get("project_id")
-        ,"status": request.form.get("status")
-        ,"concluido": request.form.get("concluido")=="on" or False
-    }
-    task = Task(**data)
-    db.session.add(task)
+    if request.form.get("id"):
+        record = Task.query.filter_by(id=request.form.get("id")).first()
+        # data['id'] = request.form.get("id")
+        # task = Task(**data)
+        record.task_name = request.form.get("task_name")
+        record.project_id = request.form.get("project_id")
+        record.status = request.form.get("status")
+        record.concluido = request.form.get("concluido")=="on" or False
+    else:
+        task = Task(**{
+            "task_name": request.form.get("task_name")
+            ,"project_id": request.form.get("project_id")
+            ,"status": request.form.get("status")
+            ,"concluido": request.form.get("concluido")=="on" or False
+            })
+        db.session.add(task)
+
     db.session.commit()
-    
-    return f"""Mensagem"""
+    return f"""<p class="alert alert-success">Dados inseridos com sucesso!<p>"""
 
 @bp.route("/tasks/lista", methods=("GET", ))
 def lista():
